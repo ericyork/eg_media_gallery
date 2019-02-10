a42bfdf76c656bc7a28bee6edd64e40d05e94060
ericyork <eyork@clarkson.edu>
2019-02-10 18:41:00 -0500

    Adds support for image modals in gallery plus fixes trigram padding

--- js/page.js
+++ js/page.js
@@ -8,3 +8,14 @@ function navToggle() {
     x.className = "topnav";
   }
 }
+//* New Modal Scripts
+$(".button").on("click", function() {
+  var modal = $(this).data("modal");
+  $(modal).show();
+});
+$(".modal").on("click", function(e) {
+  var className = e.target.className;
+  if(className === "modal" || className === "close"){
+    $(this).closest(".modal").hide();
+  }
+});
