acf67b64bb5d5b69f2ad32377c56ee770b588ced
ericyork <eyork@clarkson.edu>
2019-02-10 21:20:30 -0500

    Adds support for all grid item modal content

--- js/page.js
+++ js/page.js
@@ -8,6 +8,34 @@ function navToggle() {
     x.className = "topnav";
   }
 }
+//* Slideshow Functions
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var indicator = document.getElementsByClassName("indicator");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < indicator.length; i++) {
+      indicator[i].className = indicator[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  indicator[slideIndex-1].className += " active";
+}
+
 //* New Modal Scripts
 $(".button").on("click", function() {
   var modal = $(this).data("modal");
